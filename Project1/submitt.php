734c21a1a15a2d707bcb03d50b638382f3573f0e
Quinton <librock.quinton@gmail.com>
2025-03-01 13:19:58 -0500

    Feat: Password to Submission

--- Project1/submitt.php
+++ Project1/submitt.php
@@ -5,7 +5,15 @@
     </head>
     <body>
     <p>Thank you for your submission</p>
+    <?php
+        if(password_verify($_POST["pwd-name"], "$2y$10\$RMUPbc4ZpKMCot4fIkCzGuoB6qwXj6PsU4vfY3ZAT1KLZ9rStcdHm")){
+            
+        } else {
+            echo("Incorrect Password Entered. Please Try again.")
+        }
 
+
+    ?>
 
     </body>
 
