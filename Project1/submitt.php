fbec51dd82a1e62c585f82b3c7ded54ecc14f5e1
Quinton <librock.quinton@gmail.com>
2025-03-01 13:31:56 -0500

    Feat: Error Handling

--- Project1/submitt.php
+++ Project1/submitt.php
@@ -4,12 +4,16 @@
         <meta charset="utf-8">
     </head>
     <body>
-    <p>Thank you for your submission</p>
     <?php
+        ini_set('display_errors', 1);
+        error_reporting(E_ALL);
+
+        include("Project1.php");
+
         if(password_verify($_POST["pwd-name"], "$2y$10\$RMUPbc4ZpKMCot4fIkCzGuoB6qwXj6PsU4vfY3ZAT1KLZ9rStcdHm")){
-            
+            echo("Thank you for your submission");
         } else {
-            echo("Incorrect Password Entered. Please Try again.")
+            echo("Incorrect Password Entered. Please Try again.");
         }
 
 
