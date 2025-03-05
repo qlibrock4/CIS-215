1c961828e8217b832e9a0bfc2a46fc2d225f067d
Quinton <librock.quinton@gmail.com>
2025-03-05 16:58:15 -0500

    Fix: Remove FUnctions

--- Project1/submitt.php
+++ Project1/submitt.php
@@ -8,29 +8,29 @@
         ini_set('display_errors', 1);
         error_reporting(E_ALL);
 
-        function passwd(){
-            include("Project1.php");
-            $password = $_POST["password"];
-            if(password_verify($_POST["pw-name"], "$2y$10\$RMUPbc4ZpKMCot4fIkCzGuoB6qwXj6PsU4vfY3ZAT1KLZ9rStcdHm")){
-                echo("Password is Correct");
-            } else {
-                echo("Incorrect Password Entered. Please Try again.");
-            } elseif(is_null($password)){
-                print("<p>You need to enter a password</p>");
-            }
-
+        
+        include("Project1.php");
+        $password = $_POST["password"];
+        if(password_verify($_POST["pw-name"], "$2y$10\$RMUPbc4ZpKMCot4fIkCzGuoB6qwXj6PsU4vfY3ZAT1KLZ9rStcdHm")){
+            echo("Password is Correct");
+        } else {
+            echo("Incorrect Password Entered. Please Try again.");
+        } elseif(is_null($password)){
+            print("<p>You need to enter a password</p>");
         }
+
+        
     
 
     
-        function happyquestion(){
-            $happy = $_POST["happy"];
-            if(is_numeric($happy)){
-                print("<p>Enter Valid Statement to: What makes you happy?</p>");
-            } else {
-                print("<p>Submission Recieved</p>");
-            }
+    
+        $happy = $_POST["happy"];
+        if(is_numeric($happy)){
+            print("<p>Enter Valid Statement to: What makes you happy?</p>");
+        } else {
+            print("<p>Submission Recieved</p>");
         }
+        
 
 
         
