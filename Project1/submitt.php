6e221b6ccac331858ad85a0f6e1aa38e2134991c
Quinton <librock.quinton@gmail.com>
2025-03-05 17:26:47 -0500

    Feat: Gender validation

--- Project1/submitt.php
+++ Project1/submitt.php
@@ -11,13 +11,11 @@
         
         include("Project1.php");
         
-        $password = $_POST["password"];
+        $password = $_POST["pw-name"];
         if(password_verify($_POST["pw-name"], "$2y$10\$RMUPbc4ZpKMCot4fIkCzGuoB6qwXj6PsU4vfY3ZAT1KLZ9rStcdHm")){
             echo("Password is Correct");
-        } elseif(is_null($password)){
-            print("<p>You need to enter a password</p>");
         } else {
-            echo("Incorrect Password Entered. Please Try again.");
+            echo("Incorrect/Invalid Password Entered. Please Try again.");
         }
 
         
@@ -29,11 +27,19 @@
         if(is_numeric($happy)){
             print("<p>Enter Valid Statement to: What makes you happy?</p>");
         } else {
-            print("<p>Submission Recieved</p>");
+            print("<p>What made you happy?</p>");
+            echo($happy);
         }
         
 
-
+        $gender = $_POST["gender"];
+        if(!is_null($gender)){
+            print("<p>The gender you chose is:</p>");
+            echo($gender);
+        
+        } else {
+            echo("Enter valid gender.");
+        }
         
     ?>
 
